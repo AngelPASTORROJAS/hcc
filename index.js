4a22f269170357492cefa4cea9ccd4096db334b7
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2023-03-16 14:44:27 +0100

    feat: ajout du readme.md + ajout de commentairedans index.js

--- index.js
+++ index.js
@@ -8,28 +8,24 @@ const app = express();
 /* Creating a new database called mydatabase.db. */
 const db = new sqlite3.Database("mydatabase.db");
 
+this.db.serialize(()=>{ //execute de manière séquentiel les db.run:
+    //...
+    
+})
 /* Creating a table called users with two columns, id and name. */
 db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
 /* Inserting a new row into the users table. The first argument is the SQL statement, and the second
 argument is an array of values to be inserted into the statement. */
 db.run("INSERT INTO users (name) VALUES (?)", ["John Doe"]);
 
-/* This is a route handler. It is a function that is executed when a request is made to the /users
-route. The first argument is the route, and the second argument is the function that is executed
-when the route is requested. The function takes two arguments, req and res. The req argument is the
-request object, and the res argument is the response object. The response object has a method called
-json, which is used to send a JSON response. The db.all method is used to execute a SQL statement
-and return all rows from the result. The first argument is the SQL statement, and the second
-argument is a callback function that is executed when the statement is completed. The callback
-function takes two arguments, err and rows. The err argument is an error object, and the rows
-argument is an array of rows returned from the statement. The res.json method is used to send a JSON
-response containing the rows. */
 app.get("/users", (_req, res) => {
+  /* Selecting all the rows from the users table and returning them as JSON. */
   db.all("SELECT * FROM users", (_err, rows) => {
     res.json(rows);
   });
 });
 
+/* Telling the server to listen on port 3000. */
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
