2e3b21fbb8ae16e2bc89bb9f8e98fb9764227b7f
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2023-03-16 15:11:52 +0100

    feat: création de la classe databaseBuilder.js

---
+++ databaseBuilder.js
@@ -0,0 +1,34 @@
+const sqlite3 = require("sqlite3").verbose();
+
+class DatabaseBuilder {
+  /**
+   * The constructor function creates a new database object and assigns it to the db property of the
+   * class
+   * @param {string} filename - The filename of the database.
+   */
+  constructor(filename) {
+    this.db = new sqlite3.Database(filename);
+  }
+
+  build() {
+    this.db.serialize(() => {
+      this.db.run(
+        "CREATE TABLE IF NOT EXISTS Adherent (id_adherent INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR(255), mot_de_passe VARCHAR(255), role VARCHAR(255));"
+      );
+
+      this.db.run(
+        "CREATE TABLE IF NOT EXISTS Validation (id_validation INTEGER PRIMARY KEY AUTOINCREMENT,date_validation DATE,statut VARCHAR(255),id_adherent INT REFERENCES Adherent (id_adherent));"
+      );
+
+      this.db.run(
+        "CREATE TABLE IF NOT EXISTS Match ( id_match INTEGER PRIMARY KEY AUTOINCREMENT, date_match DATE, adversaire VARCHAR(255), score_final VARCHAR(255), id_adherent INT REFERENCES Adherent (id_adherent));"
+      );
+
+      this.db.run(
+        "CREATE TABLE IF NOT EXISTS Inscription ( id_inscription INTEGER PRIMARY KEY AUTOINCREMENT, id_adherent INT REFERENCES Adherent (id_adherent), id_match INT REFERENCES Match (id_match));"
+      );
+    });
+  }
+}
+
+module.exports = DatabaseBuilder;
--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 /* The above code is importing the express and sqlite3 modules. */
 const express = require("express");
-const sqlite3 = require("sqlite3");
-
 /* Creating an instance of the express module. */
 const app = express();
 
-/* Creating a new database called mydatabase.db. */
-const db = new sqlite3.Database("mydatabase.db");
+/* Importing the databaseBuilder module. */
+const DatabaseBuilder = require("./databaseBuilder");
+/* Creating a new database called database.db. */
+const db = new DatabaseBuilder("autre.db");
 
-this.db.serialize(()=>{ //execute de manière séquentiel les db.run:
-    //...
-    
-})
-/* Creating a table called users with two columns, id and name. */
-db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+
 /* Inserting a new row into the users table. The first argument is the SQL statement, and the second
 argument is an array of values to be inserted into the statement. */
-db.run("INSERT INTO users (name) VALUES (?)", ["John Doe"]);
+// db.run("INSERT INTO users (name) VALUES (?)", ["John Doe"]);
 
-app.get("/users", (_req, res) => {
-  /* Selecting all the rows from the users table and returning them as JSON. */
-  db.all("SELECT * FROM users", (_err, rows) => {
+app.get("/Adherent", (_req, res) => {
+  db.all("SELECT * FROM Adherent", (_err, rows) => {
+    res.json(rows);
+  });
+});
+
+app.get("/Validation", (_req, res) => {
+  db.all("SELECT * FROM Validation", (_err, rows) => {
+    res.json(rows);
+  });
+});
+
+app.get("/Match", (_req, res) => {
+  db.all("SELECT * FROM Match", (_err, rows) => {
+    res.json(rows);
+  });
+});
+
+app.get("/Inscription", (_req, res) => {
+  db.all("SELECT * FROM Inscription", (_err, rows) => {
     res.json(rows);
   });
 });
